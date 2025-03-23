09731d7c03dfbebedfd3ab8aef12af1092b3e57d
bhagya554 <kudupudi.bhagya@gmail.com>
2025-03-23 21:56:39 +0530

    03oct2024

--- src/oct/tasks/Oct032024_TableOf10.java
+++ src/oct/tasks/Oct032024_TableOf10.java
@@ -1,9 +1,10 @@
 package oct.tasks;
 
+//CTRL+ALT+L -- align/format
 public class Oct032024_TableOf10 {
     public static void main(String[] args) {
-        for(int i=1;i<=10;i++){
-            System.out.println("10 X "+i+ " = " +10*i);
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("10 X " + i + " = " + 10 * i);
         }
 
     }
